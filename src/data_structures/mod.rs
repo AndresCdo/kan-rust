e4ead6aaafc19d6cf6c6ee1de0e9371bf19db155
Andres Caicedo <andrezz1997@gmail.com>
2024-06-06 20:03:39 +0200

    chore: Update layer, Matrix, and vector modules

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -1,5 +1,7 @@
 pub mod vector;
 pub mod matrix;
+pub mod layer;
 
 pub use vector::Vector;
 pub use matrix::Matrix;
+pub use layer::Layer;
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,5 +1,3 @@
-pub mod layer;
 pub mod network;
 
-pub use layer::Layer;
 pub use network::Network;
